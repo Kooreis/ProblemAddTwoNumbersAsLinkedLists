1625402aa382210319cf303a8511b0debd491374
Kooreis <kooreis.official@gmail.com>
2024-12-07 08:15:38 -0600

    Created a LinkedList class with a constructor that initializes the head attribute. Also added methods to push data onto the list and print the list.

--- solution.py
+++ solution.py
@@ -1,4 +1,15 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, end = ' ')
+            temp = temp.next
+        print()
