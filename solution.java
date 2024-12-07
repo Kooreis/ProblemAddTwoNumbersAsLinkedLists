91360dae821a4980c15bde38d594e9d010425a3d
Kooreis <kooreis.official@gmail.com>
2024-12-07 08:15:38 -0600

    This line of the script calls the addTwoNumbers function with num1 and num2 as arguments, and assigns the result to a new LinkedList called result.

--- solution.java
+++ solution.java
@@ -1,13 +1,1 @@
-import java.util.LinkedList;
-
-public class Main {
-    public static void main(String[] args) {
-        LinkedList<Integer> num1 = new LinkedList<>();
-        num1.add(2);
-        num1.add(4);
-        num1.add(3);
-
-        LinkedList<Integer> num2 = new LinkedList<>();
-        num2.add(5);
-        num2.add(6);
-        num2.add(4);
+LinkedList<Integer> result = addTwoNumbers(num1, num2);
